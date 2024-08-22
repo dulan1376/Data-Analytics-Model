74aff1a33a538842b56fe1cd910029eb61bba6a5
dulan <matocinosdylan@gmail.com>
2024-08-22 13:26:58 +0800

    Update DecisionTreeRegressorCode.py

--- DecisionTreeRegressorModel/DecisionTreeRegressorCode.py
+++ DecisionTreeRegressorModel/DecisionTreeRegressorCode.py
@@ -61,3 +61,27 @@ val_mae = mean_absolute_error(val_y, val_predictions)
 # uncomment following line to see the validation_mae
 print(val_mae)
 
+def get_mae(max_leaf_nodes, train_X, val_X, train_y, val_y):
+    model = DecisionTreeRegressor(max_leaf_nodes=max_leaf_nodes, random_state=0)
+    model.fit(train_X, train_y)
+    preds_val = model.predict(val_X)
+    mae = mean_absolute_error(val_y, preds_val)
+    return(mae)
+
+#Compare Different Tree Sizes
+candidate_max_leaf_nodes = [5, 25, 50, 100, 250, 500]
+# Write loop to find the ideal tree size from candidate_max_leaf_nodes
+# Here is a short solution with a dict comprehension.
+# The lesson gives an example of how to do this with an explicit loop.
+scores = {leaf_size: get_mae(leaf_size, train_X, val_X, train_y, val_y) for leaf_size in candidate_max_leaf_nodes}
+ 
+# Store the best value of max_leaf_nodes (it will be either 5, 25, 50, 100, 250 or 500)
+best_tree_size = min(scores, key=scores.get)
+
+
+#Fit Model Using All Data
+# Fill in argument to make optimal size and uncomment
+final_model = DecisionTreeRegressor(max_leaf_nodes=best_tree_size, random_state=1)
+
+# fit the final model and uncomment the next two lines
+final_model.fit(X, y)
