a312c83359d1781d5e8db8ebb456347e57eaed72
dulan <matocinosdylan@gmail.com>
2024-08-22 14:42:59 +0800

    Update PandasCode.py

--- Pandas/PandasCode.py
+++ Pandas/PandasCode.py
@@ -29,3 +29,36 @@ reviews
 animals = pd.DataFrame({'Cows': [12, 20], 'Goats': [22, 19]}, index=['Year 1', 'Year 2'])
 animals
 animals.to_csv('cows_and_goats.csv')
+
+import pandas as pd
+
+reviews = pd.read_csv("../input/wine-reviews/winemag-data-130k-v2.csv", index_col=0)
+pd.set_option("display.max_rows", 5)
+
+reviews.head()
+
+desc = reviews['description']
+
+first_description = reviews['description'][0]
+
+first_row = reviews.iloc[0]
+
+first_descriptions = reviews['description'].head(10)
+first_descriptions
+
+sample_reviews = reviews.loc[[1,2,3,5,8]]
+sample_reviews
+
+df = reviews.loc[[0, 1, 10, 100], ['country', 'province', 'region_1', 'region_2']]
+df
+
+df = reviews.iloc[:100][['country', 'variety']]
+df
+
+italian_wines = reviews.loc[reviews.country == 'Italy']
+
+
+top_oceania_wines = reviews[(reviews['country'].isin(['Australia', 'New Zealand'])) & (reviews['points'] >= 95)]
+top_oceania_wines
+
+
