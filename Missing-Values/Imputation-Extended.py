22fbcd17ce92380a10a887551341c73afcbb0ad5
dulan <matocinosdylan@gmail.com>
2024-08-28 03:58:21 +0800

    Create Imputation-Extended.py

---
+++ Missing-Values/Imputation-Extended.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Load the data
+data = pd.read_csv('../input/melbourne-housing-snapshot/melb_data.csv')
+
+# Select target
+y = data.Price
+
+# To keep things simple, we'll use only numerical predictors
+melb_predictors = data.drop(['Price'], axis=1)
+X = melb_predictors.select_dtypes(exclude=['object'])
+
+# Divide data into training and validation subsets
+X_train, X_valid, y_train, y_valid = train_test_split(X, y, train_size=0.8, test_size=0.2,
+                                                      random_state=0)
+
+
+
+# Shape of training data (num_rows, num_columns)
+print(X_train.shape)
+
+# Number of missing values in each column of training data
+missing_val_count_by_column = (X_train.isnull().sum())
+print(missing_val_count_by_column[missing_val_count_by_column > 0])
